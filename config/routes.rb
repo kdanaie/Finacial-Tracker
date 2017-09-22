7f850342d0c7555d5f824ab53a15870f7d9a78e3
”Sedal <kdanaie@hotmail.com>
2017-09-22 15:43:56 +0000

    User controller and my_portfolio view

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   # root 'welcome#index'
   root 'pages#home'
 
+ get 'my_portfolio', to: 'users#my_portfolio'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
